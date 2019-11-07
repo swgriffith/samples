fa5a4dfb2edb66bb8a4b70cf46c219d89a074c61
Steve Griffith <steve.griffith@microsoft.com>
2019-11-07 16:47:43 -0500

    minor updates

--- 2.hello-kubernetes/oam-deploy/reset.sh
+++ 2.hello-kubernetes/oam-deploy/reset.sh
@@ -1,6 +1,11 @@
+# Clean out the app deployment and the component specs
 kubectl delete -f node-app-component.yaml
 kubectl delete -f python-app-component.yaml
 kubectl delete -f dapr-demo-app-config.yaml
+# Remove the deployed Services. Dapr currently does not remove.
+kubectl delete svc nodeapp-dapr
+kubectl delete svc pythonapp-dapr
+# Re-install the components and app
 kubectl create -f node-app-component.yaml
 kubectl create -f python-app-component.yaml
 kubectl create -f dapr-demo-app-config.yaml
